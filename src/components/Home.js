10bfac85506277cbb1b0376d8d0f4a34fc571a6c
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-04-04 09:58:12 -0500

    added seaport 1.4

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,10 +24,13 @@ export default function Home() {
   const tableRef = useRef(null);
 
   const openseaAddresses = [
-    "0x7f268357a8c2552623316e2562d90e642bb538e5",
-    "0x00000000006c3852cbef3e08e8df289169ede581",
-    "0x00000000006cee72100d161c57ada5bb2be1ca79",
-    "0x7be8076f4ea4a4ad08075c2508e481d6c946d12b",
+    "0x7be8076f4ea4a4ad08075c2508e481d6c946d12b", // Opensea Wyvern Exchange v1
+    "0x7f268357a8c2552623316e2562d90e642bb538e5", // Opensea Wyvern Exchange v2
+    "0x00000000006cee72100d161c57ada5bb2be1ca79", // Seaport 1.0
+    "0x00000000006c3852cbef3e08e8df289169ede581", // Seaport 1.1
+    "0x00000000000006c7676171937C444f6BDe3D6282", // Seaport 1.2
+    "0x0000000000000aD24e80fd803C6ac37206a45f15", // Seaport 1.3
+    "0x00000000000001ad428e4906aE43D8F9852d0dD6", // Seaport 1.4
   ];
 
   const { get } = useFetch("https://api.etherscan.io/api");
@@ -86,10 +89,32 @@ export default function Home() {
         // Sort data by failed TX and Opensea TX
         const failed = data.result.filter(
           (tx) =>
-            (tx.isError === "1" && tx.to.includes(openseaAddresses[0])) ||
-            (tx.isError === "1" && tx.to.includes(openseaAddresses[1])) ||
-            (tx.isError === "1" && tx.to.includes(openseaAddresses[2])) ||
-            (tx.isError === "1" && tx.to.includes(openseaAddresses[3]))
+            (tx.isError === "1" &&
+              tx.to
+                .toLowerCase()
+                .includes(openseaAddresses[0].toLowerCase())) ||
+            (tx.isError === "1" &&
+              tx.to
+                .toLowerCase()
+                .includes(openseaAddresses[1].toLowerCase())) ||
+            (tx.isError === "1" &&
+              tx.to
+                .toLowerCase()
+                .includes(openseaAddresses[2].toLowerCase())) ||
+            (tx.isError === "1" &&
+              tx.to
+                .toLowerCase()
+                .includes(openseaAddresses[3].toLowerCase())) ||
+            (tx.isError === "1" &&
+              tx.to
+                .toLowerCase()
+                .includes(openseaAddresses[4].toLowerCase())) ||
+            (tx.isError === "1" &&
+              tx.to
+                .toLowerCase()
+                .includes(openseaAddresses[5].toLowerCase())) ||
+            (tx.isError === "1" &&
+              tx.to.toLowerCase().includes(openseaAddresses[6].toLowerCase()))
         );
         setLoading(false);
         setCopied(false);
